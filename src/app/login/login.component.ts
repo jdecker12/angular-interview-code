aacd88995064f120d4697519b597905fe19e6254
jdecker12 <jeremydecker1@me.com>
2024-03-14 13:25:31 -0400

    updated login challenge

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+   //// properties ////
+   username!: string;
+   password!: string;
+ 
+   validUserName: string = 'Jeremy';
+   validPassword: string = "P@ssword";
+   
+   ///loginForm!: ElementRef;
+   @ViewChild('loginForm', { static: true }) loginForm!: NgForm;
   constructor() {}
+
+  login(): void {
+    if (this.loginForm.valid) {
+      /// compare user input with valid validUserName and validPassword for sucess or failed login
+    }
+   }
 }
+
+
